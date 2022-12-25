d9c86eac2577023237ae96d4a3bf854586e18ffa
cksghks89 <song960705@naver.com>
2022-12-26 01:37:24 +0900

    level. silver 3

---
+++ src/Boj_17626_FourSquares.java
@@ -0,0 +1,33 @@
+/*
+    Boj 17626. Four Squares
+    level. silver 3
+    solved by 송찬환
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Boj_17626_FourSquares {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(j * j > i){
+                    break;
+                }
+
+                dp[i] = Math.min(dp[i], dp[i - j*j] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
