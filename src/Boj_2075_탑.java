ca9c1051b46fa63465bbc1d7e4311496ccb249fb
ChanHwan <song960705@naver.com>
2023-02-03 23:57:57 +0900

    level. silver 3

---
+++ src/Boj_2075_탑.java
@@ -0,0 +1,46 @@
+/*
+    Boj 2075. 탑
+    level. silver 3
+    solved by 송찬환
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Boj_2493_탑 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer>[] pqList = new PriorityQueue[N];
+        for (int i = 0; i < N; i++) {
+            pqList[i] = new PriorityQueue<>((x, y) -> y - x);
+        }
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                pqList[j].add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            int idx = 0;
+            int cur = Integer.MIN_VALUE;
+            for (int j = 0; j < N; j++) {
+                if(cur < pqList[j].peek()){
+                    cur = pqList[j].peek();
+                    idx = j;
+                }
+            }
+            max = pqList[idx].poll();
+        }
+
+        System.out.println(max);
+    }
+}
