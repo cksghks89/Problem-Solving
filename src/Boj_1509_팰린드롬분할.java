dc79d3d776685e4d0b95805345be9f2636edf92d
ChanHwan <song960705@naver.com>
2023-02-13 16:04:45 +0900

    level. gold 1

--- src/Boj_1509_팰린드롬분할.java
+++ src/Boj_1509_팰린드롬분할.java
@@ -3,25 +3,18 @@
     level. gold 1
     solved by 송찬환
  */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Boj_1509_팰린드롬분할 {
-    static ArrayList<int[]> palindrome;
     static String str;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         str = " " + br.readLine();
-
-        palindrome = new ArrayList<>();
-        setAllPalindrome();
-
-        palindrome.sort((o1, o2) -> o1[1] - o2[1]);
-
         System.out.println(dp());
     }
 
@@ -30,25 +23,14 @@ public class Boj_1509_팰린드롬분할 {
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
 
-        for (int[] cur : palindrome) {
-            int s = cur[0];
-            int e = cur[1];
-
-            dp[e] = Math.min(dp[e], dp[s - 1] + 1);
+        for (int i = 1; i < str.length(); i++) {
+            for (int j = 1; j <= i; j++) {
+                if (!isPalindrome(j, i)) continue;
+                dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+            }
         }
 
         return dp[str.length() - 1];
-    }
-
-    static void setAllPalindrome() {
-        for (int i = 1; i < str.length(); i++) {
-            int idx = str.length() - 1;
-            for (int j = idx; j >= i; j--) {
-                if (isPalindrome(i, j)) {
-                    palindrome.add(new int[]{i, j});
-                }
-            }
-        }
     }
 
     static boolean isPalindrome(int start, int end) {
