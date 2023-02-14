349ca769d8304820cd9709d3c1a715bc5ad7e038
ChanHwan <song960705@naver.com>
2023-02-14 17:37:16 +0900

    level. 0

---
+++ src/programmers/Programmers_이진수더하기.java
@@ -0,0 +1,29 @@
+package programmers;
+
+public class Programmers_이진수더하기 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution("10", "11"));
+        System.out.println(sol.solution("1001", "1111"));
+    }
+
+    static class Solution {
+        public String solution(String bin1, String bin2) {
+            String answer = "";
+            int sum = binaryToDecimal(bin1) + binaryToDecimal(bin2);
+            return Integer.toBinaryString(sum);
+        }
+
+        int binaryToDecimal(String bin) {
+            int sum = 0;
+            int idx = 0;
+            for (int i = bin.length() - 1; i >= 0; i--) {
+                if (bin.charAt(i) == '1') {
+                    sum += Math.pow(2, idx);
+                }
+                idx++;
+            }
+            return sum;
+        }
+    }
+}
---
+++ src/programmers/Programmers_편지.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class Programmers_편지 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution("happy birthday!"));
+    }
+    static class Solution {
+        public int solution(String message) {
+            return message.length() * 2;
+        }
+    }
+}
