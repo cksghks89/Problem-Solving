51c21a8b72b6f5a5b76f9a14d95d01317e6503e4
ChanHwan <song960705@naver.com>
2023-03-15 23:23:32 +0900

    level. silver 2

---
+++ src/Boj_11048_이동하기.java
@@ -0,0 +1,35 @@
+/*
+    Boj 11048. 이동하기
+    level. silver 2
+    solved by 송찬환
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_11048_이동하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j - 1], Math.max(dp[i - 1][j], dp[i][j - 1])) + map[i - 1][j - 1];
+            }
+        }
+        System.out.println(dp[N][M]);
+    }
+}
