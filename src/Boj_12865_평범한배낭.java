84eb2d2b8bb6814a29f6771da4b4650581586afd
ChanHwan <song960705@naver.com>
2023-03-22 21:10:38 +0900

    level gold 5

--- src/Boj_12865_평범한배낭.java
+++ src/Boj_12865_평범한배낭.java
@@ -1,5 +1,5 @@
 /*
-    Boj 12865. 평번한 배낭
+    Boj 12865. 평범한 배낭
     level. gold 5
     solved by 송찬환
  */
@@ -11,34 +11,23 @@ import java.util.StringTokenizer;
 public class Boj_12865_평범한배낭 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[][] items = new int [N][2];
+        int[] dp = new int[K + 1];
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            items[i][0] = Integer.parseInt(st.nextToken());
-            items[i][1] = Integer.parseInt(st.nextToken());
-        }
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
-        int[][] dp = new int[N+1][K + 1];
-        for(int i = 0; i < N; i++){
-            for(int j = 1; j <= K; j++){
-                if(items[i][0] > j){
-                    dp[i+1][j] = dp[i][j];
-                }else{
-                    dp[i+1][j] = Math.max(dp[i][j], dp[i][j - items[i][0]] + items[i][1]);
-                }
+            for (int j = K; j >= w; j--) {
+                dp[j] = Math.max(dp[j], dp[j - w] + v);
             }
         }
 
-        int rtn = Integer.MIN_VALUE;
-        for(int cur : dp[N]){
-            rtn = Math.max(rtn, cur);
-        }
-        System.out.println(rtn);
+        System.out.println(dp[K]);
     }
 }
