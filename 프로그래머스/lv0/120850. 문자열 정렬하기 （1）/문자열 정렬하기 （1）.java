1f6022f074ca569bd2bf7f94d1a8fb6c2abeeb8f
ChanHwan <song960705@naver.com>
2023-04-01 20:49:43 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 3.95 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String my_string) {
+        return Arrays.stream(my_string.replaceAll("[a-zA-Z]", "").split("")).mapToInt(Integer::parseInt).sorted().toArray();
+    }
+}
