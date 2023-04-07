6f41361def5b496e309f659b9d8fea7de7cb06bb
ChanHwan <song960705@naver.com>
2023-04-07 09:52:38 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.86 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    Map<String, Integer> str1Map;
+    Map<String, Integer> str2Map;
+
+    public int solution(String str1, String str2) {
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        str1Map = makeMap(str1);
+        str2Map = makeMap(str2);
+
+        int retain = 0;
+        int union = 0;
+
+        for (Map.Entry<String, Integer> entry : str1Map.entrySet()) {
+            if (!entry.getKey().matches("^([a-z])([a-z])$")) continue;
+
+            if (str2Map.containsKey(entry.getKey())) {
+                retain += Math.min(entry.getValue(), str2Map.get(entry.getKey()));
+                union += Math.max(entry.getValue(), str2Map.get(entry.getKey()));
+                str2Map.remove(entry.getKey());
+            } else {
+                union += entry.getValue();
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : str2Map.entrySet()) {
+            if (!entry.getKey().matches("^([a-z])([a-z])$")) continue;
+            union += entry.getValue();
+        }
+
+        int answer = 65536;
+        if (union != 0) {
+            answer = (int) (1.0 * retain / union * 65536);
+        }
+
+        return answer;
+    }
+
+    private Map<String, Integer> makeMap(String str) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            String cur = str.substring(i, i + 2);
+            map.put(cur, map.getOrDefault(cur, 0) + 1);
+        }
+        return map;
+    }
+}
