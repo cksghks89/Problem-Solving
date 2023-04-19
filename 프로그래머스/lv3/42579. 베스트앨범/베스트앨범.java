fe6a75a29d3e216fe3e6fd613fa5a70832996edc
ChanHwan <song960705@naver.com>
2023-04-19 23:34:03 +0900

    [level 3] Title: 베스트앨범, Time: 1.12 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
@@ -8,8 +8,8 @@ class Solution {
             genreTime.put(genres[i], genreTime.getOrDefault(genres[i], 0) + plays[i]);
         }
 
-        // 1, 2, 3 번 조건으로 정렬한 값을 저장하는 TreeSet 자료구조
-        TreeSet<Integer> set = new TreeSet<>((o1, o2) -> {
+        // 1, 2, 3 번 조건으로 정렬한 값을 저장하는 pq 자료구조
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
             if (!genres[o1].equals(genres[o2])) {
                 return genreTime.get(genres[o2]) - genreTime.get(genres[o1]);
             } else if (plays[o1] != plays[o2]) {
@@ -19,13 +19,14 @@ class Solution {
             }
         });
 
-        // TreeSet 에 고유번호 넣기
-        for (int i = 0; i < genres.length; i++) set.add(i);
+        // pq 에 고유번호 넣기
+        for (int i = 0; i < genres.length; i++) pq.add(i);
 
         // 장르별 2개씩만 결과 배열에 담기
         List<Integer> result = new ArrayList<>();
         Map<String, Integer> cnt = new HashMap<>();
-        for (int idx : set) {
+        while(!pq.isEmpty()) {
+            int idx = pq.poll();
             if (!cnt.containsKey(genres[idx]) || cnt.get(genres[idx]) < 2) {
                 cnt.put(genres[idx], cnt.getOrDefault(genres[idx], 0) + 1);
                 result.add(idx);
