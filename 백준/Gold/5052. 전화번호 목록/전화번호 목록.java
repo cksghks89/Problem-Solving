8105079321c6450de9fb4acb515d30e20492bb93
ChanHwan <song960705@naver.com>
2024-03-17 15:12:22 +0900

    [Gold IV] Title: 전화번호 목록, Time: 364 ms, Memory: 108508 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -29,8 +29,7 @@ public class Main {
             boolean result = true;
 
             for (int i = 0; i < n; i++) {
-                int[] arr = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
-                if (!insert(arr, 0, root, false)) {
+                if (!insert(br.readLine(), 0, root, false)) {
                     result = false;
                 }
             }
@@ -42,18 +41,20 @@ public class Main {
         System.out.println(sb);
     }
 
-    private static boolean insert(int[] arr, int cnt, Node node, boolean created) {
+    private static boolean insert(String arr, int cnt, Node node, boolean created) {
         if (node.isEnd) return false;
-        if (cnt == arr.length) {
+        if (cnt == arr.length()) {
             node.isEnd = true;
             return created;
         }
 
-        if (node.child[arr[cnt]] == null) {
-            node.child[arr[cnt]] = new Node(arr[cnt]);
-            return insert(arr, cnt + 1, node.child[arr[cnt]], true);
+        int cur = arr.charAt(cnt) - '0';
+
+        if (node.child[cur] == null) {
+            node.child[cur] = new Node(cur);
+            return insert(arr, cnt + 1, node.child[cur], true);
         } else {
-            return insert(arr, cnt + 1, node.child[arr[cnt]], created);
+            return insert(arr, cnt + 1, node.child[cur], created);
         }
     }
 }
