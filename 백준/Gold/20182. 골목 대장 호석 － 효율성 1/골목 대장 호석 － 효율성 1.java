2fd2a1af989ad5dd37170e979ab59ec100844d8d
ChanHwan <song960705@naver.com>
2024-08-15 22:01:37 +0900

    [Gold III] Title: 골목 대장 호석 - 효율성 1, Time: 1344 ms, Memory: 223720 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.java
+++ 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.java
@@ -3,67 +3,96 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.parseInt;
+
 public class Main {
-    private static final int INF = 110_000_000;
-    private static int N, M, A, B, C;
-    private static List<int[]>[] graph;
+    static int[] dist;
+    static List<List<Node>> graph = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = parseInt(st.nextToken());
+        int M = parseInt(st.nextToken());
+        int A = parseInt(st.nextToken());
+        int B = parseInt(st.nextToken());
+        int C = parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        A = Integer.parseInt(st.nextToken());
-        B = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        dist = new int[N + 1];
+        graph.add(null);
+        for (int i = 0; i < N; i++)
+            graph.add(new ArrayList<>());
 
-        graph = new List[N + 1];
-        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
-
-        for (int i = 0; i < M; i++) {
+        int u, v, w;
+        while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+            u = parseInt(st.nextToken());
+            v = parseInt(st.nextToken());
+            w = parseInt(st.nextToken());
 
-            graph[s].add(new int[]{e, w});
-            graph[s].add(new int[]{s, w});
+            graph.get(u).add(new Node(v, w));
+            graph.get(v).add(new Node(u, w));
         }
 
-        int answer = -1;
-        for (int i = 1; i <= 20; i++) {
-            boolean result = dijkstra(i);
-            if (result) {
-                answer = i;
-                break;
-            }
-        }
-
-        System.out.println(answer);
+        System.out.println(binSearch(A, B, C));
+        br.close();
     }
 
-    private static boolean dijkstra(int max) {
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, INF);
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        pq.offer(new int[]{A, 0});
-        dist[A] = 0;
+    static int binSearch(int start, int end, int C) {
+        int low = 1;
+        int high = 21;
+        int mid;
+        int answer = -1;
 
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
+        while (low <= high) {
+            mid = (low + high) / 2;
 
-            for (int i = 0; i < graph[cur[0]].size(); i++) {
-                int[] next = graph[cur[0]].get(i);
-
-                if (next[1] <= max && cur[1] + next[1] < dist[next[0]]) {
-                    dist[next[0]] = cur[1] + next[1];
-                    pq.offer(new int[]{next[0], dist[next[0]]});
-                }
+            if (dijkstra(start, end, C, mid)) {
+                answer = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
-        return dist[B] <= C;
+        return answer;
+    }
+
+
+    static boolean dijkstra(int start, int end, int C, int x) {
+        Arrays.fill(dist, MAX_VALUE);
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(n -> n.w));
+        dist[start] = 0;
+        pq.offer(new Node(start, dist[start]));
+
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+
+            if (cur.v == end) return dist[cur.v] <= C;
+
+            if (dist[cur.v] < cur.w) continue;
+
+            for (Node next : graph.get(cur.v)) {
+                if (next.w > x) continue;
+
+                if (dist[next.v] < dist[cur.v] + next.w) continue;
+
+                dist[next.v] = dist[cur.v] + next.w;
+                pq.offer(new Node(next.v, dist[next.v]));
+            }
+        }
+
+        return false;
+    }
+
+    static class Node {
+        int v, w;
+
+        public Node(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
     }
 }
